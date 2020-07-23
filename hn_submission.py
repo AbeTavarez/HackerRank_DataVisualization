1c3666ad67d1cc21e1906f61936d5294b9f22f5b
Abraham E. Tavarez <abetavarez@icloud.com>
2020-07-23 19:02:09 -0400

    getting all comments

--- hn_submission.py
+++ hn_submission.py
@@ -7,26 +7,26 @@ req = requests.get(url)
 print(f"Status code: {req.status_code}")
 
 
-# process information about each submission
+# Process information about each submission
 submission_ids = req.json()
 submission_dicts = []
 
 for submission_id in submission_ids[:30]:
     # Make a separate API call for each submission
-    url = "https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
+    url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
     req = requests.get(url)
     print(f"id: {submission_id}\tstatus: {req.status_code}")
-    reponce_dict = req.json()
+    responce_dict = req.json()
     # Buils a dictionary for each article
     submission_dict = {
-        "title": reponce_dict,
+        "title": responce_dict["title"],
         "hn_link": f"https://news.ycombinator.com/item?id={submission_id}",
-        "comments": responce_dict["desendants"],
+        "comments": responce_dict.get("descendants", 0),
     }
     # Appends to list
     submission_dicts.append(submission_dict)
-
-submission_dicts - sorted(submission_dicts,
+print(submission_dicts)
+submission_dicts = sorted(submission_dicts,
                           key=itemgetter("comments"), reverse=True)
 
 for submission_dict in submission_dicts:
